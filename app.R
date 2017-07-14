f2d4841ace1574d3e4dad2996f40f5558e0b06a7
MD Dubey <mddubey409@gmail.com>
2017-07-14 12:37:32 +0530

    Dubey | Moved out bar and charts tab as a module.

--- app.R
+++ app.R
@@ -5,9 +5,9 @@
 #                           "ggplot2", "scales", "eeptools", "data.table",
 #                           "dplyr","DT","shinyjs","shinyBS","purrr",
 #                           "lazyeval","DescTools")
-# 
+#
 # remove.packages(pkgs_to_remove)
-install.packages('RMySQL', type='source')
+install.packages('RMySQL', type = 'source')
 if (!"properties" %in% rownames(installed.packages()))
   install.packages("properties")
 if (!"install.load" %in% rownames(installed.packages()))
@@ -16,20 +16,36 @@ library(install.load)
 if (!"devtools" %in% rownames(installed.packages()))
   install.packages("devtools")
 library(devtools)
-if (!"DBI" %in% rownames(installed.packages())) 
+if (!"DBI" %in% rownames(installed.packages()))
   devtools::install_github("rstats-db/DBI")
-if (!"pool" %in% rownames(installed.packages())) 
+if (!"pool" %in% rownames(installed.packages()))
   devtools::install_github("rstudio/pool")
-if (!"shiny" %in% rownames(installed.packages())) 
+if (!"shiny" %in% rownames(installed.packages()))
   devtools::install_github("rstudio/shiny")
 library(DBI)
 library(shiny)
 #install the required packages
-pkgs_to_install_load <- c("tidyr", "stringr", "readr","lubridate", "RMySQL","readr",
-                          "ggplot2", "scales", "eeptools", "data.table",
-                          "dplyr","DT","shinyjs","shinyBS","purrr",
-                          "lazyeval","DescTools")
-sapply(pkgs_to_install_load,install_load)
+pkgs_to_install_load <-
+  c(
+    "tidyr",
+    "stringr",
+    "readr",
+    "lubridate",
+    "RMySQL",
+    "readr",
+    "ggplot2",
+    "scales",
+    "eeptools",
+    "data.table",
+    "dplyr",
+    "DT",
+    "shinyjs",
+    "shinyBS",
+    "purrr",
+    "lazyeval",
+    "DescTools"
+  )
+sapply(pkgs_to_install_load, install_load)
 
 
 
@@ -55,7 +71,7 @@ sapply(pkgs_to_install_load,install_load)
 # library(purrr)
 # library(lazyeval)
 # library(DescTools)
-options(shiny.trace=F)
+options(shiny.trace = F)
 
 source("connector.R")
 source("ui.R")
@@ -64,24 +80,28 @@ source("serverModules.R")
 
 pool <- getConnectionPool()
 observationTabNS <- "observation"
+barChartsTabNS <- "barChart"
 
 ui <- fluidPage(
   useShinyjs(),
   tags$head(tags$style(".rightAlign{float:right;}")),
   tags$head(tags$style(".btnbottomAlign{margin-top: 25px;}")),
-  bsButton("inExplorer", label = "Files",
-           block = F, type = "toggle", value = TRUE),
+  bsButton(
+    "inExplorer",
+    label = "Files",
+    block = F,
+    type = "toggle",
+    value = TRUE
+  ),
   pageWithSidebar(
     headerPanel("Welcome!"),
     sideBarUI("sideBar"),
-    mainPanel(
-      tabsetPanel(
-        id="inTabPanel",
-        tabPanel('Observations', observationTabUI(observationTabNS)),
-        tabPanel('Bars and Charts',barChartTabUI()),
-        tabPanel('Dashboard')
-      )
-    )
+    mainPanel(tabsetPanel(
+      id = "inTabPanel",
+      tabPanel('Observations', observationTabUI(observationTabNS)),
+      tabPanel('Bars and Charts', barChartTabUI(barChartsTabNS)),
+      tabPanel('Dashboard')
+    ))
   )
 )
 
@@ -89,131 +109,70 @@ server <- function(input, output, session) {
   main_table <- reactiveValues(data = NULL)
   main_plot <- reactiveValues(data = NULL)
   table_data <- reactiveValues(data = NULL)
-  selectChoices <- reactiveValues(data = list("Class" = 1, "Question" = 2, "Answer" = 3))
+  selectChoices <-
+    reactiveValues(data = list(
+      "Class" = 1,
+      "Question" = 2,
+      "Answer" = 3
+    ))
   
   callModule(sideBar, "sideBar")
-  callModule(observationTab,observationTabNS, pool, main_table)
-
+  callModule(observationTab, observationTabNS, pool, main_table)
+  callModule(barChartTab,
+             barChartsTabNS,
+             main_table,
+             table_data,
+             main_plot)
+  
   dateTimeConcepts <- getDateTimeConcepts(input, pool)
   conceptForSelection <- getConceptForSelection(input, pool)
   conceptAnswers <- getConceptAnswers(input, pool)
-
+  
   observeEvent(input$inExplorer, {
-    if(input$inExplorer){shinyjs::show(id = "sideBar-inSaveSideBar")}
-    else{shinyjs::hide(id = "sideBar-inSaveSideBar")}
+    if (input$inExplorer) {
+      shinyjs::show(id = "sideBar-inSaveSideBar")
+    }
+    else{
+      shinyjs::hide(id = "sideBar-inSaveSideBar")
+    }
   })
   
   observeEvent(input$inTabPanel, {
-    if(input$inTabPanel=="Bars and Charts"){
-      updateCheckboxGroupInput(session, 
-                               "inDimensions",
-                               choices = names(main_table$data),
-                               selected = NULL
+    if (input$inTabPanel == "Bars and Charts") {
+      updateCheckboxGroupInput(
+        session,
+        "barChart-inDimensions",
+        choices = names(main_table$data),
+        selected = NULL
       )
-      updateSelectInput(session, 
-                        "inCharts",
-                        choices = list("Table"=1, "Bar Chart"=2, "Histogram"=3,"Scatter Plot"=4)
+      updateSelectInput(
+        session,
+        "barChart-inCharts",
+        choices = list(
+          "Table" = 1,
+          "Bar Chart" = 2,
+          "Histogram" = 3,
+          "Scatter Plot" = 4
+        )
       )
       
     }
     else{
       # Can also set the label and select items
-      updateSelectInput(session, "observation-inSelect",
-                        label = "",
-                        choices = selectChoices$data,
-                        selected = 2
+      updateSelectInput(
+        session,
+        "observation-inSelect",
+        label = "",
+        choices = selectChoices$data,
+        selected = 2
       )
-      updateSelectInput(session, "observation-inDateBy",
+      updateSelectInput(session,
+                        "observation-inDateBy",
                         label = "Date Filter",
-                        choices = dateTimeConcepts()
-      )
+                        choices = dateTimeConcepts())
       
     }
   })
-
-  #scatter plot ranges
-  scatter_ranges <- reactiveValues(x = NULL, y = NULL)
-  # When a double-click happens, check if there's a brush on the plot.
-  # If so, zoom to the brush bounds; if not, reset the zoom.
-  observeEvent(input$scatter_dblclick, {
-    brush <- input$scatter_brush
-    if (!is.null(brush)) {
-      scatter_ranges$x <- c(brush$xmin, brush$xmax)
-      scatter_ranges$y <- c(brush$ymin, brush$ymax)
-      
-    } else {
-      scatter_ranges$x <- NULL
-      scatter_ranges$y <- NULL
-    }
-  })
-  #Charts and Graphs
-  observeEvent(input$inShow, {
-    chartOption <- input$inCharts
-    grp_cols <- input$inDimensions
-    obs <- main_table$data
-    dots <- lapply(grp_cols, as.symbol)
-    if(chartOption == 1){
-      tableop <- ftable(droplevels(obs[grp_cols]))
-      table_data$data <- tableop
-      output$tableDF <- renderTable(as.matrix(tableop),rownames = T)
-      selectedValue <- "Table" 
-    }else if(chartOption == 2){ #barchart
-      output$barPlot <- renderPlot({
-        bar_1 <- obs %>% ggplot(aes_string(grp_cols[1]))
-        bar_1 <- bar_1 +  geom_bar()
-        main_plot$data <- bar_1 + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-        main_plot$data
-      })
-      selectedValue <- "Bar Chart" 
-    }else if(chartOption == 3){ #histogram
-      output$histPlot <- renderPlot({
-        hist_1 <- obs %>% ggplot(aes_string(grp_cols[1]))
-        main_plot$data <- hist_1 +  geom_histogram(binwidth = input$inHistInput)
-        main_plot$data
-      })
-      selectedValue <- "Histogram" 
-    }else if(chartOption == 4){ #scatter plot
-      output$scatterPlot <- renderPlot({
-        scatter_plot <- obs %>% ggplot(aes_string(x = grp_cols[1], y = grp_cols[2], col = "Gender"))
-        scatter_plot <- scatter_plot + geom_point()
-        main_plot$data <- scatter_plot + coord_cartesian(xlim = scatter_ranges$x, ylim = scatter_ranges$y)
-        main_plot$data
-      })
-      selectedValue <- "Scatter Plot" 
-    }
-    updateNavbarPage(session,"inChartMenu", selected = selectedValue )
-    output$tableDownload <- renderUI({
-      tagList(
-        downloadButton('downloadTable', 'Download'),
-        actionButton("inAddtoDB","Add to Dashboard")
-      )
-    })
-  })
-
-  #Download Table
-  output$downloadTable <- downloadHandler(
-    filename = function() { 
-      chartOption <- input$inCharts
-      fextn <- ifelse(chartOption ==1, ".csv",".png")
-      fprefix <- ifelse(chartOption ==1, "table","plot")
-      paste(fprefix, "_",year(ymd_hms(Sys.time())),
-            "_",month(ymd_hms(Sys.time())),
-            "_",day(ymd_hms(Sys.time())),
-            "_",hour(ymd_hms(Sys.time())),
-            "_",minute(ymd_hms(Sys.time())),
-            "_",second(ymd_hms(Sys.time())),
-            fextn, sep='') 
-    },
-    content = function(file) {
-      chartOption <- input$inCharts
-      if(chartOption == 1){
-        op_df <- stats:::format.ftable(table_data$data, quote=FALSE)
-        write.csv(op_df, file)
-      }else{
-        ggsave(file, plot = main_plot$data, device = "png" )
-      }
-    }
-  )
 }
 
 shinyApp(ui = ui, server = server)
--- ui.R
+++ ui.R
@@ -1,157 +1,178 @@
 sideBarUI <- function(id) {
-	ns <- NS(id)
-	div(id =ns("inSaveSideBar"),sidebarPanel(
-	  width = 3,
-	  actionButton(ns("inSave"),"Save"),
-	  p(""),
-	  checkboxGroupInput(ns('inDocuments'), 'Files:',
-	                     choices=c(""))
-	))	
-}
-
-observationTabUI <- function(id){
   ns <- NS(id)
-	tagList(
-         p(""),
-         fluidRow(
-           column(2,selectInput(ns("inSelect"), "",
-                                c(""), selected = 2))
-           ,
-           column(4,selectInput(ns("inCheckboxGroup"), "",
-                                c(""), multiple = T))
-           ,
-           conditionalPanel(
-             condition = paste("input[['",id,"-inSelect']]==2", sep=""),
-             column(4,selectInput(ns("inAnswers"), "",
-                                  c(""), multiple = T))
-           )
-         ),
-         fluidRow(
-           column(4,selectInput(ns("inDateBy"), "Date Filter",
-                                c("")))
-           ,
-           column(4,dateRangeInput(ns("inDateRange"),
-                                   label = 'Range',
-                                   start = Sys.Date() -30, end = Sys.Date() 
-           ))
-         ),
-         actionButton(ns("inApply"),"Apply"),
-         #what is this part doing?
-         conditionalPanel(
-           condition = "1==0",
-           checkboxInput(ns("incheckbox"), label = "Choice A", value = F)
-         ),
-         #what is this part doing?
-         p(""),
-         conditionalPanel(
-           condition = paste("input[['",id,"-incheckbox']]==1", sep=""),
-           bsButton(ns("inShowColumns"), label = "Hide Column Selection",
-                    block = F, type = "toggle", value = TRUE),
-           p(""),
-           sidebarLayout(
-             div(id =ns("inColumnNamePanel"),sidebarPanel(
-               width = 3,
-               checkboxGroupInput(ns("inColumnNames"), 'Columns:',
-                                  choices=c(""))
-             )),
-             mainPanel(
-               conditionalPanel(
-                 condition = paste("input[['",id,"-incheckbox']]==1", sep="") ,
-                     bsCollapse(id = ns("inCollapseAddCols"),
-                                bsCollapsePanel("Add Columns", fluidRow(
-                                  column(4,
-                                         selectizeInput(ns("inNumericCols"),"Numeric Columns:", choices=c(""))
-                                         ),
-                                  column(2,
-                                         numericInput(ns("inStartRange"),"Start", value="")
-                                        ),
-                                  column(2,
-                                         numericInput(ns("inEndRange"),"End", value="")
-                                        ),
-                                  column(2,
-                                         textInput(ns("inLevelName"),"Name", value="")
-                                        ),
-                                  column(1,
-                                         actionButton(ns("inAddLevel"), label = "+", class = 'btnbottomAlign')
-                                        ) 
-                                ),
-                                fluidRow(
-                                  column(3,
-                                         selectizeInput(ns("inCatLevels"),"Levels:", choices=c(""))
-                                        ),
-                                  column(3,
-                                         actionButton(ns("inApplyColumn"),"Apply", class = 'btnbottomAlign')
-                                        )
-                                ),style = "info")
-                     )
-               ),
-               DT::dataTableOutput(ns("obsDT"))
-               ,
-               downloadButton(ns("downloadData"), 'Download')
-             )
-           )
-         )
-		)
+  div(
+    id = ns("inSaveSideBar"),
+    sidebarPanel(
+      width = 3,
+      actionButton(ns("inSave"), "Save"),
+      p(""),
+      checkboxGroupInput(ns('inDocuments'), 'Files:',
+                         choices = c(""))
+    )
+  )
 }
 
-barChartTabUI <- function() {
-	tagList(
-	 p(""),
-	 pageWithSidebar(
-	   
-	   # Application title
-	   headerPanel(""),
-	   
-	   # Sidebar with a slider input
-	   sidebarPanel(
-	     checkboxGroupInput('inDimensions', 'Columns:',
-	                        choices=c("")),
-	     selectInput('inCharts', 'Charts:',
-	                        choices=c("Table", "Bar Chart", "Histogram", "Scatter Plot"), multiple = F),
-	     actionButton("inShow","Show")
-	   ),
-	   # Show a plot of the generated distribution
-	   mainPanel(
-	     navbarPage("", id = "inChartMenu",
-	                navbarMenu("Output",
-	                           tabPanel("Table",
-	                                    tableOutput("tableDF")
-	                           ),
-	                           tabPanel("Bar Chart",
-	                                    fluidRow(
-	                                      column(12,
-	                                             plotOutput("barPlot")
-	                                      )
-	                                    )
-	                           ),
-	                           tabPanel("Histogram",
-	                                    fluidRow(
-	                                      column(12,
-	                                             sliderInput("inHistInput", "Choose a bin width:", 1, 100, 10)
-	                                      )
-	                                    ),
-	                                    fluidRow(
-	                                      column(12,
-	                                             plotOutput("histPlot")
-	                                      )
-	                                    )
-	                           ),
-	                           tabPanel("Scatter Plot",
-	                                    fluidRow(
-	                                      column(12,
-	                                             plotOutput("scatterPlot",
-	                                                        dblclick = "scatter_dblclick",
-	                                                        brush = brushOpts(
-	                                                          id = "scatter_brush",
-	                                                          resetOnNew = TRUE
-	                                                        ))
-	                                      )
-	                                    )
-	                           )
-	                )
-	     ),
-	     uiOutput("tableDownload")
-	   )
-	 )
-	)
+observationTabUI <- function(id) {
+  ns <- NS(id)
+  tagList(
+    p(""),
+    fluidRow(
+      column(2, selectInput(ns("inSelect"), "",
+                            c(""), selected = 2))
+      ,
+      column(4, selectInput(
+        ns("inCheckboxGroup"), "",
+        c(""), multiple = T
+      ))
+      ,
+      conditionalPanel(
+        condition = paste("input[['", id, "-inSelect']]==2", sep = ""),
+        column(4, selectInput(ns("inAnswers"), "",
+                              c(""), multiple = T))
+      )
+    ),
+    fluidRow(column(4, selectInput(
+      ns("inDateBy"), "Date Filter",
+      c("")
+    ))
+    ,
+    column(
+      4,
+      dateRangeInput(
+        ns("inDateRange"),
+        label = 'Range',
+        start = Sys.Date() - 30,
+        end = Sys.Date()
+      )
+    )),
+    actionButton(ns("inApply"), "Apply"),
+    #what is this part doing?
+    conditionalPanel(condition = "1==0",
+                     checkboxInput(
+                       ns("incheckbox"), label = "Choice A", value = F
+                     )),
+    #what is this part doing?
+    p(""),
+    conditionalPanel(
+      condition = paste("input[['", id, "-incheckbox']]==1", sep = ""),
+      bsButton(
+        ns("inShowColumns"),
+        label = "Hide Column Selection",
+        block = F,
+        type = "toggle",
+        value = TRUE
+      ),
+      p(""),
+      sidebarLayout(
+        div(
+          id = ns("inColumnNamePanel"),
+          sidebarPanel(width = 3,
+                       checkboxGroupInput(
+                         ns("inColumnNames"), 'Columns:',
+                         choices = c("")
+                       ))
+        ),
+        mainPanel(
+          conditionalPanel(
+            condition = paste("input[['", id, "-incheckbox']]==1", sep = "") ,
+            bsCollapse(
+              id = ns("inCollapseAddCols"),
+              bsCollapsePanel(
+                "Add Columns",
+                fluidRow(
+                  column(4,
+                         selectizeInput(
+                           ns("inNumericCols"), "Numeric Columns:", choices = c("")
+                         )),
+                  column(2,
+                         numericInput(ns("inStartRange"), "Start", value =
+                                        "")),
+                  column(2,
+                         numericInput(ns("inEndRange"), "End", value =
+                                        "")),
+                  column(2,
+                         textInput(ns("inLevelName"), "Name", value =
+                                     "")),
+                  column(1,
+                         actionButton(
+                           ns("inAddLevel"), label = "+", class = 'btnbottomAlign'
+                         ))
+                ),
+                fluidRow(column(
+                  3,
+                  selectizeInput(ns("inCatLevels"), "Levels:", choices =
+                                   c(""))
+                ),
+                column(
+                  3,
+                  actionButton(ns("inApplyColumn"), "Apply", class = 'btnbottomAlign')
+                )),
+                style = "info"
+              )
+            )
+          ),
+          DT::dataTableOutput(ns("obsDT"))
+          ,
+          downloadButton(ns("downloadData"), 'Download')
+        )
+      )
+    )
+  )
+}
+
+barChartTabUI <- function(id) {
+  ns <- NS(id)
+  tagList(p(""),
+          pageWithSidebar(
+            # Application title
+            headerPanel(""),
+            
+            # Sidebar with a slider input
+            sidebarPanel(
+              checkboxGroupInput(ns("inDimensions"), 'Columns:',
+                                 choices = c("")),
+              selectInput(
+                ns("inCharts"),
+                'Charts:',
+                choices = c("Table", "Bar Chart", "Histogram", "Scatter Plot"),
+                multiple = F
+              ),
+              actionButton(ns("inShow"), "Show")
+            ),
+            # Show a plot of the generated distribution
+            mainPanel(navbarPage(
+              "",
+              id = ns("inChartMenu"),
+              navbarMenu(
+                "Output",
+                tabPanel("Table",
+                         tableOutput(ns("tableDF"))),
+                tabPanel("Bar Chart",
+                         fluidRow(column(
+                           12,
+                           plotOutput(ns("barPlot"))
+                         ))),
+                tabPanel("Histogram",
+                         fluidRow(column(
+                           12,
+                           sliderInput(ns("inHistInput"), "Choose a bin width:", 1, 100, 10)
+                         )),
+                         fluidRow(column(
+                           12,
+                           plotOutput(ns("histPlot"))
+                         ))),
+                tabPanel("Scatter Plot",
+                         fluidRow(column(
+                           12,
+                           plotOutput(
+                             ns("scatterPlot"),
+                             dblclick = ns("scatter_dblclick"),
+                             brush = brushOpts(id = ns("scatter_brush"),
+                                               resetOnNew = TRUE)
+                           )
+                         )))
+              )
+            ),
+            uiOutput(ns("tableDownload")))
+          ))
 }
