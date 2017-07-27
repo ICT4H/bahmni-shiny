6121742a566db9956fba4dd5b2183f177833f482
MD Dubey <mddubey409@gmail.com>
2017-07-27 11:48:31 +0530

    Dubey, Chethan | Fetching data on visit level.

--- plugins/hypertension/dao.R
+++ plugins/hypertension/dao.R
@@ -16,23 +16,29 @@ fetchData <- function(pool, startDate, endDate) {
   variablesToFetch <- list("BMI", "Systolic blood pressure",
                            "Diastolic blood pressure")
 
-  choleraConceptId <- pool %>%
+  hypertensionConceptId <- pool %>%
      tbl("concept_name") %>%
-     filter(voided == 0, name=="Cholera",
+     filter(voided == 0, name=="Hypertension",
       concept_name_type=="FULLY_SPECIFIED") %>%
      select(concept_id) %>%
      pull(concept_id)
 
-  patientWithObs <- pool %>% 
+  codedDiagnosisConceptId <- pool %>%
+     tbl("concept_name") %>%
+     filter(voided == 0, name=="Coded Diagnosis",
+      concept_name_type=="FULLY_SPECIFIED") %>%
+     select(concept_id) %>%
+     pull(concept_id)
+
+  patientWithHypertension <- pool %>% 
     tbl("obs") %>% 
     filter(voided==0,
-     value_coded == choleraConceptId,
-     obs_datetime>=startDate,
-     obs_datetime<endDate) %>% 
-    select(person_id, concept_id, obs_id, value_numeric) %>% 
+     value_coded == hypertensionConceptId,
+     concept_id == codedDiagnosisConceptId) %>% 
+    select(person_id, encounter_id, concept_id, obs_id, value_numeric) %>% 
     collect(n = Inf)
 
-  personIds <- pull(patientWithObs, person_id)
+  personIds <- pull(patientWithHypertension, person_id)
   patients <- pool %>%
     tbl("person") %>%
     filter(voided==0, person_id %in% personIds) %>%
@@ -58,12 +64,35 @@ fetchData <- function(pool, startDate, endDate) {
   #Block Below need to be modified when we have an idea
   #about filters for visit_type and obs_relation 
 
-  patientVisitDates <- pool %>% 
-    tbl("visit") %>% 
+  # visitsForHypertension <- pool %>%
+  #   tbl("encounter") %>%
+  #   filter(voided==0,
+  #     encounter_id %in% pull(patientWithHypertension, encounter_id)) %>%
+  #   select(visit_id) %>%
+  #   collect(n=Inf)
+
+  # encountersForHypertensionVisits <- pool %>%
+  #   tbl("encounter") %>%
+  #   filter(voided == 0,
+  #     visit_id %in% visitsForHypertension) %>%
+  #   select(encounter_id, visit_id) %>%
+  #   collect(n=Inf)
+  encounters <- pool %>%
+    tbl("encounter") %>%
     filter(voided==0, patient_id %in% personIds) %>% 
-    group_by(patient_id) %>%
-    summarise(date_started = max(date_started)) %>%
-    select(patient_id, date_started) %>%
+    select(patient_id, encounter_id, visit_id) %>%
+    rename(patientId=patient_id) %>%
+    collect(n=Inf)
+
+  visits <- pool %>%
+    tbl("visit") %>%
+    filter(voided==0, patient_id %in% personIds) %>% 
+    select(patient_id, visit_id, date_started) %>%
+    collect(n=Inf)
+
+  patientVisits <- encounters %>%
+    inner_join(visits, by=c("visit_id"="visit_id")) %>%
+    select(patient_id, encounter_id, visit_id, date_started) %>%
     rename(visitDate = date_started) %>%
     collect(n=Inf)
 
@@ -72,10 +101,9 @@ fetchData <- function(pool, startDate, endDate) {
     rename(District = county_district) %>%
     rename(State = state_province) %>%
     inner_join(patientIdentifiers, by = c("person_id"="patient_id")) %>%
-    inner_join(patientVisitDates, by = c("person_id"="patient_id")) %>%
     collect(n=Inf)
 
-  allObsForCholeraPatients <- pool %>%
+  allObsForHypertensionPatients <- pool %>%
     tbl("obs") %>%
     filter(voided==0, person_id %in% personIds) %>%
     collect(n=Inf)
@@ -85,16 +113,16 @@ fetchData <- function(pool, startDate, endDate) {
     filter(voided == 0, name %in% variablesToFetch,
       concept_name_type=="FULLY_SPECIFIED") %>%
     select(concept_id,name) %>%
-    rename(conceptid = concept_id) %>%
     collect(n=Inf) 
 
-  obsForVariables <- allObsForCholeraPatients %>%
-    inner_join(conceptNames, by = c("concept_id"="conceptid")) %>%  
+  obsForVariables <- allObsForHypertensionPatients %>%
+    inner_join(conceptNames, by = c("concept_id"="concept_id")) %>%  
     inner_join(patients, by = c("person_id"="person_id")) %>%
-    rename(ID=identifier) %>%
-    group_by(name,person_id) %>%
+    inner_join(patientVisits, by=c("encounter_id"="encounter_id")) %>%
+    group_by(visit_id, patient_id, concept_id) %>%
     filter(obs_datetime == max(obs_datetime)) %>%
     ungroup() %>%
+    rename(ID=identifier) %>%
     select(ID,name,obs_datetime,value_numeric, Age, State, District, Gender, visitDate) %>%
     collect(n = Inf)
 
